66225a672feb8bcd87334fa29a45bfa0d1d8097d
Emrie <emrie@emries-mbp.mynetworksettings.com>
2023-04-25 02:14:03 -0400

    added the form

--- afterlifeproject/src/App.js
+++ afterlifeproject/src/App.js
@@ -4,21 +4,149 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>HelloWorld</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1> The After Life</h1>
+    <p>What will we return as? Wouldn't you like to know?</p>
+
+    <form>
+  <div>
+    <p>Question 1:</p>
+    <label>
+      <input type="radio" name="q1" value="option1" />
+      Option 1
+    </label>
+    <label>
+      <input type="radio" name="q1" value="option2" />
+      Option 2
+    </label>
+    <label>
+      <input type="radio" name="q1" value="option3" />
+      Option 3
+    </label>
+  </div>
+  <div>
+    <p>Question 2:</p>
+    <label>
+      <input type="radio" name="q2" value="option1" />
+      Option 1
+    </label>
+    <label>
+      <input type="radio" name="q2" value="option2" />
+      Option 2
+    </label>
+    <label>
+      <input type="radio" name="q2" value="option3" />
+      Option 3
+    </label>
+  </div>
+  <div>
+    <p>Question 3:</p>
+    <label>
+      <input type="radio" name="q3" value="option1" />
+      Option 1
+    </label>
+    <label>
+      <input type="radio" name="q3" value="option2" />
+      Option 2
+    </label>
+    <label>
+      <input type="radio" name="q3" value="option3" />
+      Option 3
+    </label>
+  </div>
+  <div>
+    <p>Question 4:</p>
+    <label>
+      <input type="radio" name="q4" value="option1" />
+      Option 1
+    </label>
+    <label>
+      <input type="radio" name="q4" value="option2" />
+      Option 2
+    </label>
+    <label>
+      <input type="radio" name="q4" value="option3" />
+      Option 3
+    </label>
+  </div>
+  <div>
+    <p>Question 5:</p>
+    <label>
+      <input type="radio" name="q5" value="option1" />
+      Option 1
+    </label>
+    <label>
+      <input type="radio" name="q5" value="option2" />
+      Option 2
+    </label>
+    <label>
+      <input type="radio" name="q5" value="option3" />
+      Option 3
+    </label>
+  </div>
+  <div>
+    <p>Question 6:</p>
+    <label>
+      <input type="radio" name="q6" value="option1" />
+      Option 1
+    </label>
+    <label>
+      <input type="radio" name="q6" value="option2" />
+      Option 2
+    </label>
+    <label>
+      <input type="radio" name="q6" value="option3" />
+      Option 3
+    </label>
+  </div>
+  <div>
+    <p>Question 7:</p>
+    <label>
+      <input type="radio" name="q7" value="option1" />
+      Option 1
+    </label>
+    <label>
+      <input type="radio" name="q7" value="option2" />
+      Option 2
+      </label>
+      <label>
+      <input type="radio" name="q7" value="option3" />
+      Option 3
+    </label>
     </div>
+    <div>
+    <p>Question 8:</p>
+    <label>
+      <input type="radio" name="q8" value="option1" />
+      Option 1
+    </label>
+    <label>
+      <input type="radio" name="q8" value="option2" />
+      Option 2
+      </label>
+      <label>
+      <input type="radio" name="q8" value="option3" />
+      Option 3
+    </label>
+    </div>
+    <div>
+    <p>Question 9:</p>
+    <label>
+      <input type="radio" name="q9" value="option1" />
+      Option 1
+    </label>
+    <label>
+      <input type="radio" name="q9" value="option2" />
+      Option 2
+      </label>
+      <label>
+      <input type="radio" name="q9" value="option3" />
+      Option 3
+    </label>
+    </div>
+    </form>
+    </div>
+    
+
   );
 }
 
