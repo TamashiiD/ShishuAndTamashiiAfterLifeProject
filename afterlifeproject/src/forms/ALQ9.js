7afee52d4ba421b6ec474d17a7590bf70b916c7d
Emrie <emrie@emries-mbp.mynetworksettings.com>
2023-07-03 21:24:45 -0400

    added the picture to the form

--- afterlifeproject/src/forms/ALQ9.js
+++ afterlifeproject/src/forms/ALQ9.js
@@ -1,30 +1,50 @@
 import { NavLink } from "react-router-dom"
 import { connect } from "react-redux"
 import { onbutton, offButton, radioChange } from "../FormState/actionCreators"
+import { useState } from "react"
 
 const ALQ9 = (props) => {
 
-    const { onbutton, offButton, radioChange } = props
+    // const { onbutton, offButton, radioChange } = props
+   const [image, setImage] = useState("")
+   const [button , setButton] =useState(true)
 
-    const valueOfChoice = localStorage.getItem("Question9")
-
-    const handleChange = (e) => {
-        radioChange(e.target.name, e.target.value)
-        onbutton("button9", valueOfChoice)
-
-        localStorage.setItem(e.target.name, e.target.value)
+    const previewImage = (e) => {
+        setButton(false)
+       e.preventDefault()
+       console.log(e.target.files[0])
+       const src = URL.createObjectURL(e.target.files[0])
+       setImage(src)
+       
+        
+    
+    }
+    
+    const handlesubmit = (e) => {
+        e.preventDefault()
+        
 
     }
-    const handlebuttontoggle = (e) => {
-        offButton(e.target.value, valueOfChoice)
-    }
+
+    // const valueOfChoice = localStorage.getItem("Question9")
+
+    // const handleChange = (e) => {
+    //     radioChange(e.target.name, e.target.value)
+    //     onbutton("button9", valueOfChoice)
+
+    //     localStorage.setItem(e.target.name, e.target.value)
+
+    // }
+    // const handlebuttontoggle = (e) => {
+    //     offButton(e.target.value, valueOfChoice)
+    // }
     return (
 
         <div className="App">
             <form>
                 <div>
                     <p>Question 9:</p>
-                    <label>
+                    {/* <label>
                         <input type="radio" checked={valueOfChoice === "option1" ? true : false} name="Question9" value="option1" onChange={handleChange} />
                         Option 1
                     </label>
@@ -35,13 +55,23 @@ const ALQ9 = (props) => {
                     <label>
                         <input type="radio" checked={valueOfChoice === "option3" ? true : false} name="Question9" value="option3" onChange={handleChange} />
                         Option 3
+                    </label> */}
+
+                    <label>
+                        <input type="file" accept="image/*" capture="camera" onChange={previewImage} />
                     </label>
+
+                    {image.length > 0 ? <div> <img className="makeitsmaller" src={image} alt="slefie" /> </div> : <div> Please Uploade an Image </div>}
+                    <div>
+                        <button onClick={handlesubmit} disabled={button} >CLICK HERE TO SEE RESULTS</button>
+                    </div>
+                 
                 </div>
             </form>
 
-            <nav>
+            {/* <nav>
                 <NavLink to="10"><button disabled={props.togglebutton.button9} onClick={handlebuttontoggle}>NEXT</button></NavLink>&nbsp;
-            </nav>
+            </nav> */}
         </div>
     )
 }
